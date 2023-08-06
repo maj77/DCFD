42fcb06c7dc2214150bf68d2d51895f73494dad8
maj77 <marcinmaj@student.agh.edu.pl>
2023-08-06 11:53:04 +0200

    linear interpolation script

--- matlab/CFD.m
+++ matlab/CFD.m
@@ -36,3 +36,26 @@ legend('original', 'original delayed', 'zero cross val', 'zero cross pulse')
 val_fxp = fi(val, 0, 32, 0);
 val_fxp_hex = hex(val_fxp);
 
+%% INTERPOLATION
+zc_len = size(zero_cross_val);
+time_extd = 1:1:zc_len(2);
+
+t = 0.5;
+zc_val_1 = zero_cross_val(1:865);
+zc_val_2 = zero_cross_val(2:866);
+
+lerp = zc_val_1 + t*(zc_val_2-zc_val_1);
+
+
+interpolated = zeros(1,866+855);
+for n=1:865
+    interpolated(2*n-1) = zero_cross_val(n);
+    interpolated(2*n)   = lerp(n);
+end
+interp_len = size(interpolated);
+time2 = 1:1:interp_len(2);
+
+
+%plot(time_extd(1:865), zero_cross_val(1:865), time_extd(1:865), lerp);
+%legend('zero cross pulse', 'interpolation pulse');
+plotyy(time2, interpolated,time_extd, zero_cross_val);
